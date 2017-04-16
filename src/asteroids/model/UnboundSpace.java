0d0d29e7cbe562637b745c9287262b482eae1e8b
teemu <jaspervanmeerbeeck@hotmail.com>
2017-04-16 13:41:15 +0200

    collision done, unboundSpace variabelen gewijzigd

--- src/asteroids/model/UnboundSpace.java
+++ src/asteroids/model/UnboundSpace.java
@@ -2,11 +2,11 @@ package asteroids.model;
 
 import be.kuleuven.cs.som.annotate.Raw;
 
-@Value
+
 public class UnboundSpace extends Space{
 	
 	@Raw
-	public UnboundSpace(double width,double height){
-		super(width,height);
+	public UnboundSpace(){
+		super(Double.POSITIVE_INFINITY,Double.POSITIVE_INFINITY);
 	}
 }
