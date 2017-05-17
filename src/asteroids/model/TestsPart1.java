e5eaeadc2593ddd1d1237d00886b37df244621cc
ambervancamp <amber.vancamp@nsint.com>
2017-05-17 20:18:49 +0200

    Verkeerde merge in orde gemaakt

--- src/asteroids/model/TestsPart1.java
+++ src/asteroids/model/TestsPart1.java
@@ -107,13 +107,8 @@ public class TestsPart1 {
 		firstShip.placeInSpace(world); 
 		secondShip.placeInSpace(world);
 		thirdShip.placeInSpace(world);
-<<<<<<< HEAD
-		assertEquals(3, world.getCertainEntities(ShipExpression).size());
-		assertEquals(0, world.getBullets().size());
-=======
 		assertEquals(3, firstShip.getCertainEntities(world).size());
 		assertEquals(0, firstBullet.getCertainEntities(world).size());
->>>>>>> branch 'master' of git@github.com:ambervancamp/OGP_project.git
 	}
 	
 
