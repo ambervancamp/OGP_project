92c72d6262b45c64bc12784ac672cb9aa600fb9c
ambervancamp <amber.vancamp@nsint.com>
2017-05-18 12:49:33 +0200

    Statement

--- src/asteroids/model/Programs/Statement.java
+++ src/asteroids/model/Programs/Statement.java
@@ -1,5 +1,29 @@
 package asteroids.model.Programs;
 
-public class Statement {
+import asteroids.part3.programs.SourceLocation;
 
+public class Statement {
+	
+	private SourceLocation sourceLocation;
+	private Program program;
+	
+	public Statement(SourceLocation sourceLocation){
+		this.setSourceLocation(sourceLocation);
+	}
+
+	public Program getProgram() {
+		return this.program;
+	}
+
+	public void setProgram(Program program) {
+		this.program = program;
+	}
+
+	public SourceLocation getSourceLocation() {
+		return this.sourceLocation;
+	}
+
+	public void setSourceLocation(SourceLocation sourceLocation) {
+		this.sourceLocation = sourceLocation;
+	}
 }
